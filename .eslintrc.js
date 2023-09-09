263bccff1d197dcc9a6be05a816860ffabb11897
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-09-09 14:19:25 -0500

    Fix eslint config and fix formatting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+  plugins: ['jest'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    'jest/globals': true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": "airbnb-base",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': false,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+};
--- index.js
+++ index.js
@@ -31,11 +31,13 @@ module.exports = {
    * @prop {EthernetProtocolVariant} protocol - the Ethernet protocol used to encode this frame
    * @prop {Object} header - the header information from the frame
    * @prop {Buffer} header.destinationMAC - the binary MAC address of the frame destination
-   * @prop {string} header.destination - a human-readable string representation of the destination MAC address
+   * @prop {string} header.destination - a human-readable string representation of the destination
+  *                                      MAC address
    * @prop {Buffer} header.sourceMAC - the binary MAC address of the frame source
    * @prop {string} header.source - a human-readable string representation of the source MAC address
    * @prop {number} header.type - the Ethertype value from the frame header
-   * @prop {number} header.length - the frame payload length without the header or frame check sequence field
+   * @prop {number} header.length - the frame payload length without the header or frame check
+   *                                sequence field
    * @prop {Object?} header.llc - (IEEE 802.3 frames only) the LLC field data from the header
    * @prop {number} header.llc.dsap - the DSAP field value from the header
    * @prop {number} header.llc.ssap - the SSAP field value from the header
@@ -43,16 +45,18 @@ module.exports = {
    * @prop {Buffer?} header.snap_oui - (IEEE 802.3 frames only) the SNAP OUI field value
    * @prop {Buffer} payload - the payload data of the frame
    * @prop {number} frame_check - the frame check sequence value contained on the end of the frame
-   * @prop {boolean} frame_check_valid - a boolean indicating whether the frame check validation succeeded
+   * @prop {boolean} frame_check_valid - a boolean indicating whether the frame check validation
+   *                                     succeeded
    */
 
   /**
    * Parse an Ethernet packet
    * @param data {Buffer} - the packet as binary data
-   * @param protocol_variant {EthernetProtocolVariant} - the variant of the Ethernet Protocol being parsed
+   * @param protocol_variant {EthernetProtocolVariant} - the variant of the Ethernet Protocol being
+   *                                                     parsed
    * @returns {ParsedEthernetFrame} - parsed fields
    */
-  parse: function (data, protocol_variant) {
+  parse(data, protocol_variant) {
     /* Implement the parsing here */
-  }
+  },
 };
